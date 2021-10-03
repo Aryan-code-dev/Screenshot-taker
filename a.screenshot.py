1347b3b2ddaa53959e52700877a7d9d6df215375
Aryan Dande <86364775+Aryan-code-dev@users.noreply.github.com>
2021-10-03 13:24:35 +0530

    Update a.screenshot.py
    Comments added

--- a.screenshot.py
+++ a.screenshot.py
@@ -3,14 +3,15 @@ import time
 import pyautogui
 import cv2
 import numpy as np
+#Input from the user
 frequency = int(input("Enter the duration between every screenshot (IN SECONDS)"))
 duration = int(input("Enter the time period in minutes  for which the program should run"))
-file_location = str(input("Enter the folder location in which the "))
+file_location = str(input("Enter the folder location where the screenshots should be saved "))
 time_input = str(input("Enter the time at which program should start in HH:MM format"))
 duration = duration*60
 loop = duration/frequency
 
-
+#The job of taking screenshot
 def job():
 
     i = 0
@@ -21,9 +22,9 @@ def job():
         i += 1
         time.sleep(frequency)
 
-
+#scheduler
 schedule.every().day.at(time_input).do(job)
-
+#schedules all pending tasks
 while True:
     schedule.run_pending()
 
